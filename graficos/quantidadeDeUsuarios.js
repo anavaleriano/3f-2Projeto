ebb5c2c769dae6e0e60ae2be15fade563ad59d69
Ana Beatriz Valeriano Araújo <101740970+anavaleriano@users.noreply.github.com>
2024-10-10 09:43:07 -0300

    10/10/2024

---
+++ graficos/quantidadeDeUsuarios.js
@@ -0,0 +1,17 @@
+async function quantidadeDeUsuarios(){
+  const url= 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+  cosnt res = await fetch(url);
+  const dados = await res.json();
+  const nomeDasRedes = Object.keys(dados);
+  const quantidadeDeUsuarios = Object.values(dados)
+
+
+  const infos = [
+    {
+      x: nomeDasRedes,
+      y: quantidadeDeUsuarios,
+      type: 'bar'
+    }
+  ]
+
+}
