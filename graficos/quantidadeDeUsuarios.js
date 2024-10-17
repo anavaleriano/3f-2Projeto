9bd86b3e860194dde84bdacbac03f1b58ed2d234
Ana Beatriz Valeriano Araújo <101740970+anavaleriano@users.noreply.github.com>
2024-10-17 09:34:40 -0300

    Update quantidadeDeUsuarios.js

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -14,7 +14,7 @@ async function quantidadeDeUsuarios(){
       y: quantidadeDeUsuarios,
       type: 'bar',
       marker: {
-        color: getCSS(--cor-primaria)
+        color: getCSS("--cor-primaria")
       }
 
     }
